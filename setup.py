edb2727aa23153b62a411eab327a700d2b3f9c29
Kye <kye@apacmediasolutions.com>
2023-05-24 14:38:23 -0400

    new setup name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'Sophia',
+  name = 'Sophia-Optimizer',
   packages = find_packages(exclude=[]),
   version = '0.1.1',
   license='APACHE',
